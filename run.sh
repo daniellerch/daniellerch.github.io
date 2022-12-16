ddf206a5006083477ec0cab6b4bf5d73bdd0a39c
Daniel Lerch <dlerch@gmail.com>
2022-12-16 10:27:25 +0100

    dev

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+#sudo apt install ruby
+#sudo apt install ruby-dev
+#sudo gem3.0 install racc
+#sudo bundle3.0 install
+
+
 #gem2.7 update --system
 #bundle2.7 install
 #bundle2.7 exec jekyll serve
-bundle2.5 exec jekyll serve
+#bundle2.5 exec jekyll serve
+bundle3.0 exec jekyll serve
